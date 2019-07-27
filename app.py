af889e4327d162667572e1f35a9ff93ad8921780
gavindon <530893850@qq.com>
2019-07-27 11:31:26 +0800

    Procfile 一个字也不能错

--- app.py
+++ app.py
@@ -13,5 +13,10 @@ def hello_test():
     return 'hello 大猪蹄子'
 
 
+@app.route("/bb")
+def hello_test():
+    return 'dear羊！ 俺想你'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
